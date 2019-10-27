fb63df8cc14b47c00326da540f3080e4cf99c800
alejolo311 <1147@holbertonschool.com>
2019-10-27 21:00:52 +0000

    adding the basic funcs

--- _functions.c
+++ _functions.c
@@ -1,11 +1,17 @@
-void print_percen(char s)
+void print_percen(char *s)
 {
+	_putchar('%');
 }
 
-void print_char(char s)
+void print_char(char *s)
 {
+
 }
 
 void print_string(char *s)
 {
+	char *s = va_arg(args, char *);
+	int len = strlen(s);
+
+	write(1, s, len);
 }
