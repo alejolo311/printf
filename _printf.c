c2db99d4643ae02c4b1f47cecae522034de78f49
juanmarcoscabezas <juanmarcoscabezas@gmail.com>
2019-10-27 20:48:28 +0000

    create the basic structure of printf and fix the header file

--- _printf.c
+++ _printf.c
@@ -1,18 +1,37 @@
 #include "holberton.h"
-
 /**
  * _printf - Printf function copy
  * Description: This is a modified version of printf
  * @s: String to print
  * Return: Number of characters printed
  */
-int _printf(char *s, ...)
+int _printf(const char *format, ...)
 {
-	pf ops[] =
+	char *pf;
+	void (*f)();
+
+	va_list args;
+
+	va_start(args, format);
+
+	for (pf = format; *pf; pf++)
 	{
-		{'%', print_percen},
-		{'c', print_char},
-		{'s', print_string},
-	};
-	
+		if(*pf == '%')
+		{
+			f = verify_format(pf);
+			if (f)
+			{
+				f(args);
+				pf++;
+			}
+			else
+			{
+				_putchar(*pf);
+			}
+		}
+		else
+			_putchar(*pf);
+	}
+	va_end(args);
+	return (0);
 }
