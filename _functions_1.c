15f81f16e3ee5cd30767d0c50e11e4133183d437
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-10-28 14:31:40 +0000

    Adding print_number function

--- _functions_1.c
+++ _functions_1.c
@@ -1,6 +1,50 @@
 #include "holberton.h"
 
 /**
+ * print_number - this function print integers
+ * @n: the integer of the entry
+ * a blank line
+ * Description: this function prints print integers?
+ * Return: a void function, no return
+ */
+int print_number(int n)
+{
+	int  k, l, len = 0;
+
+	k = 1;
+	l = n;
+	if (n < 0)
+	{
+		k = k * -1;
+		_putchar('-');
+		len++;
+	}
+	while (l > 9 || l < -9)
+	{
+		l = l / 10;
+		k = k * 10;
+	}
+
+	while (k > 9 || k < -9)
+	{
+		_putchar((n / k) % 10 + '0');
+		k = k / 10;
+		len++;
+	}
+	if (n < 0)
+	{
+		_putchar((n % 10)  * -1 + '0');
+		len++;
+	}
+	else
+	{
+		_putchar(n % 10 + '0');
+		len++;
+	}
+	return (len);
+}
+
+/**
  * print_d - Prints integers
  * Description: This function prints integers
  * @args: Argument to print
@@ -8,10 +52,12 @@
  */
 int print_d(va_list args)
 {
-	char s = va_arg(args, int);
+	int number, len;
 
-	_putchar(s);
-	return (1);
+	number = va_arg(args, int);
+	len = print_number(number);
+
+	return (len);
 }
 
 /**
@@ -22,18 +68,10 @@ int print_d(va_list args)
  */
 int print_i(va_list args)
 {
-	char *s = va_arg(args, char *);
-	int len;
+	int number, len;
 
-	if (s)
-	{
-		len = strlen(s);
-		write(1, s, len);
-	}
-	else
-	{
-		len = 6;
-		write(1, "(null)", 6);
-	}
+	number = va_arg(args, int);
+	len = print_number(number);
+
 	return (len);
 }
