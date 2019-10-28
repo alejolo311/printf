d71cbe15f22bad5689e120b80a81885997a13ce4
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-10-28 17:09:30 +0000

    Creating functions for unsigned, octal, hexadecimal and hexadecimal uppercase

---
+++ _functions_4.c
@@ -0,0 +1,136 @@
+#include "holberton.h"
+
+/**
+ * print_number_u - this function print unsigned integers
+ * @n: the integer of the entry
+ * Description: this function prints print integers
+ * Return: a void function, no return
+ */
+int print_number_u(unsigned int n)
+{
+	int  k, l, len = 0;
+
+	k = 1;
+	l = n;
+	if (n < 0)
+	{
+		k = k * -1;
+		_putchar('-');
+		len++;
+	}
+	while (l > 9 || l < -9)
+	{
+		l = l / 10;
+		k = k * 10;
+	}
+
+	while (k > 9 || k < -9)
+	{
+		_putchar((n / k) % 10 + '0');
+		k = k / 10;
+		len++;
+	}
+	if (n < 0)
+	{
+		_putchar((n % 10)  * -1 + '0');
+		len++;
+	}
+	else
+	{
+		_putchar(n % 10 + '0');
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * print_u - Prints unsigned integers
+ * Description: This function prints unsigned integers
+ * @args: Argument to print
+ * Return: Length of @args
+ */
+int print_u(va_list args)
+{
+	unsigned int number;
+	int len;
+
+	number = va_arg(args, unsigned int);
+	len = print_number_u(number);
+
+	return (len);
+}
+
+/**
+ * print_o - Prints octals
+ * Description: This function prints octals
+ * @args: Argument to print
+ * Return: length of @args
+ */
+int print_o(va_list args)
+{
+	int number, len, i;
+	int result[32];
+
+	number = va_arg(args, int);
+	if (number == 0)
+	{
+		print_number(0);
+		return (1);
+	}
+	for (len = 0; number > 0; len++)
+	{
+		result[len] = number % 8;
+		number = number / 8;
+	}
+	return (len);
+}
+
+/**
+ * print_x - Prints hexadecimals
+ * Description: This function prints hexadecimals
+ * @args: Argument to print
+ * Return: length of @args
+ */
+int print_x(va_list args)
+{
+	int number, len, i;
+	int result[32];
+
+	number = va_arg(args, int);
+	if (number == 0)
+	{
+		print_number(0);
+		return (1);
+	}
+	for (len = 0; number > 0; len++)
+	{
+		result[len] = number % 8;
+		number = number / 8;
+	}
+	return (len);
+}
+
+/**
+ * print_X - Prints hexadecimals in uppercase
+ * Description: This function prints hexadecimals in uppsercase
+ * @args: Argument to print
+ * Return: length of @args
+ */
+int print_X(va_list args)
+{
+	int number, len, i;
+	int result[32];
+
+	number = va_arg(args, int);
+	if (number == 0)
+	{
+		print_number(0);
+		return (1);
+	}
+	for (len = 0; number > 0; len++)
+	{
+		result[len] = number % 8;
+		number = number / 8;
+	}
+	return (len);
+}
