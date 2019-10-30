80a39039ed4d56567c37b97c32d1081f09de788a
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-10-30 00:19:00 +0000

    Working on print pointers

--- _functions_7.c
+++ _functions_7.c
@@ -1,26 +1,61 @@
 #include "holberton.h"
+
+/**
+ * print_hex - Prints hexadecimals
+ * Description: This function prints hexadecimals
+ * @number: Number to print
+ * @buffer: Buffer
+ * Return: length of @args
+ */
+int print_hex(unsigned long number, char *buffer)
+{
+	long len, i, result[32];
+
+	if (number == 0)
+	{
+		print_number(0, buffer);
+		return (1);
+	}
+	for (len = 0; number > 0; len++)
+	{
+		result[len] = number % 16;
+		number = number / 16;
+	}
+	for (i = len - 1; i >= 0; i--)
+	{
+		if (result[i] >= 10)
+			_putchar(buffer, result[i] + 87);
+		else
+			_putchar(buffer, result[i] + 48);
+	}
+	return (len);
+}
+
+/**
+ * print_p - Prints address of memory
+ * Description: This function prints memmory addresses
+ * @args: Argument to print
+ * @buffer: Buffer
+ * Return: Length of @args
+ */
 int print_p(va_list args, char *buffer)
 {
-	char *s = va_arg(args, long);
-	int len, i;
+	int len = 2, i;
+	char *s;
+	unsigned long p = (unsigned long) va_arg(args, void *);
 
-	if (s)
-	{/*
-		len = strlen(s);
-		for (i = 0; i < len; i++)
-		{
-			_putchar(buffer, '0');
-			c++;
-			_putchar(buffer, 'x');
-		c += print_hexa(, buffer);
-		}*/
+	if (p)
+	{
+		_putchar(buffer, '0');
+		_putchar(buffer, 'x');
+		len += print_hex(p, buffer);
 	}
 	else
 	{
-		len = 6;
-		s = "(null)";
+		len = 5;
+		s = "(nil)";
 		for (i = 0; i < len; i++)
 			_putchar(buffer, *(s + i));
-			}
-	return (0);
+	}
+	return (len);
 }
